c2e3e1f7aed52a358e0ccd6f71acd5b9258cf49a
felipeog <felipeog476@gmail.com>
2022-01-23 16:31:45 -0300

    Update hook

--- src/hooks/useTipCalculator.ts
+++ src/hooks/useTipCalculator.ts
@@ -1,33 +1,33 @@
 import { useState, useMemo } from 'react'
 
 export function useTipCalculator() {
-  const [bill, setBill] = useState<number>()
-  const [tipPercentage, setTipPercentage] = useState<number>()
-  const [numberOfPeople, setNumberOfPeople] = useState<number>()
+  const [bill, setBill] = useState<number | string>('')
+  const [tipPercentage, setTipPercentage] = useState<number | string>('')
+  const [numberOfPeople, setNumberOfPeople] = useState<number | string>('')
 
   const tipPerPerson = useMemo(() => {
     if (numberOfPeople <= 0) {
       return 0
     }
 
-    if (!tipPercentage || isNaN(tipPercentage)) {
+    if (!tipPercentage || isNaN(+tipPercentage)) {
       return 0
     }
 
-    const tip = bill * (tipPercentage / 100)
+    const tip = +bill * (+tipPercentage / 100)
 
-    return tip / numberOfPeople
+    return tip / +numberOfPeople
   }, [bill, tipPercentage, numberOfPeople])
   const totalPerPerson = useMemo(() => {
     if (numberOfPeople <= 0) {
       return 0
     }
 
-    return bill / numberOfPeople + tipPerPerson
+    return +bill / +numberOfPeople + tipPerPerson
   }, [bill, numberOfPeople, tipPerPerson])
 
   return {
-    value: {
+    get: {
       bill,
       tipPercentage,
       numberOfPeople,
