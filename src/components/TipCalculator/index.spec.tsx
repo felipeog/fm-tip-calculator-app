3c98bedbba6385eab4bf636b5f08b3ae384a8f40
felipeog <felipeog476@gmail.com>
2022-02-12 15:01:21 -0300

    Update TipCalculator tests

--- src/components/TipCalculator/index.spec.tsx
+++ src/components/TipCalculator/index.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 
 import { TipCalculator, TIP_PERCENTAGES } from './index'
@@ -81,6 +82,57 @@ describe('test TipCalculator component', () => {
       render(<TipCalculator />)
 
       expect(screen.getByText('Reset')).toBeInTheDocument()
+    })
+  })
+
+  describe('events test', () => {
+    it('test calculation with tip button', () => {
+      expect.hasAssertions()
+
+      render(<TipCalculator />)
+
+      const billValue = '100'
+      const peopleValue = '2'
+      const billInput = screen.getAllByRole('spinbutton')[0]
+      const peopleInput = screen.getAllByRole('spinbutton')[2]
+      const tipButton = screen.getAllByRole('button')[0]
+
+      userEvent.type(billInput, billValue)
+      userEvent.type(peopleInput, peopleValue)
+      userEvent.click(tipButton)
+
+      expect(billInput).toHaveValue(Number(billValue))
+      expect(peopleInput).toHaveValue(Number(peopleValue))
+      expect(tipButton).toHaveClass('Button--selected')
+      expect(screen.getByText('$2.50')).toBeInTheDocument()
+      expect(screen.getByText('$52.50')).toBeInTheDocument()
+    })
+
+    it('test calculation with custom tip input', () => {
+      expect.hasAssertions()
+
+      render(<TipCalculator />)
+
+      const tipValue = '10'
+      const tipInput = screen.getAllByRole('spinbutton')[1]
+
+      userEvent.type(tipInput, tipValue)
+
+      expect(tipInput).toHaveValue(Number(tipValue))
+      expect(screen.getByText('$5.00')).toBeInTheDocument()
+      expect(screen.getByText('$55.00')).toBeInTheDocument()
+    })
+
+    it('test reset button', () => {
+      expect.hasAssertions()
+
+      render(<TipCalculator />)
+
+      const resetButton = screen.getByText('Reset')
+
+      userEvent.click(resetButton)
+
+      expect(screen.getAllByText('$0.00')).toHaveLength(2)
     })
   })
 })
