1239853ce040507bf9c97eb3d19b8e1704f6238b
felipeog <felipeog476@gmail.com>
2022-02-14 00:54:57 -0300

    Add stories

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,4 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { within, userEvent } from '@storybook/testing-library'
+import { useState } from 'react'
 
 import { Button } from './index'
 
@@ -9,14 +11,47 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
+export const Default = Template.bind({})
+Default.args = {
+  variant: 'light',
+  isSelected: false,
+  isDisabled: false,
+  className: 'stories-button',
+  children: 'Button',
+}
+
 export const Disabled = Template.bind({})
 Disabled.args = {
+  ...Default.args,
   isDisabled: true,
-  children: 'Disabled',
 }
 
 export const Selected = Template.bind({})
 Selected.args = {
+  ...Default.args,
   isSelected: true,
-  children: 'Selected',
+}
+
+export const ToggleOnClick: ComponentStory<typeof Button> = (args) => {
+  const [isSelected, setIsSelected] = useState(false)
+
+  function toggleSelected() {
+    setIsSelected((prevIsSelected) => !prevIsSelected)
+  }
+
+  return <Button {...args} isSelected={isSelected} onClick={toggleSelected} />
+}
+ToggleOnClick.args = {
+  ...Default.args,
+}
+
+export const OnClick = Template.bind({})
+OnClick.args = {
+  ...Default.args,
+  onClick: () => console.log('onClick'),
+}
+OnClick.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+
+  await userEvent.click(canvas.getByText('Button'))
 }
